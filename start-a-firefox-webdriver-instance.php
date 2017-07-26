aa0a942e2abf8626efa66c896c0250b97a6bb24c
Ethan Mallove <mallove@fas.harvard.edu>
2017-07-26 17:02:49 -0400

    add start-a-firefox-webdriver-instance.php
    Per this thread https://stackoverflow.com/questions/26386418/how-to-use-attach-an-existing-browser-using-selenium

---
+++ start-a-firefox-webdriver-instance.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Facebook\WebDriver;
+ 
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+ 
+require_once('vendor/autoload.php');
+ 
+$host = 'http://localhost:4445/wd/hub'; // this is the default
+$capabilities = DesiredCapabilities::firefox();
+$driver = RemoteWebDriver::create($host, $capabilities, 5000);  # Connection refused?
+
+var_dump($driver);
