9c1fc94103a1bb956c70a4aceb5bcd1be01afaba
Ethan Mallove <mallove@fas.harvard.edu>
2017-07-30 00:21:37 -0400

    Checkpoint before trying SauceLabs
    Checkpoint before trying SauceLabs SC Sauce Connect proxy.
    (Put the selenium server in the cloud. Is a selenium-server jar
    file so hard to run locally?  Seems heavy duty for running a few
    behat/cucumber tests.)

--- start-a-firefox-webdriver-instance.php
+++ start-a-firefox-webdriver-instance.php
@@ -2,6 +2,9 @@
 
 namespace Facebook\WebDriver;
 
+
+use Facebook\WebDriver\Chrome\ChromeDriver;
+use Facebook\WebDriver\Firefox;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
  
@@ -9,5 +12,20 @@ require_once('vendor/autoload.php');
 
 $host = 'http://localhost:4445/wd/hub'; // this is the default
 $capabilities = DesiredCapabilities::firefox();
+//$capabilities = DesiredCapabilities::chrome();
+//$capabilities = DesiredCapabilities::htmlUnit();
+
+//$capabilities->setCapability('acceptSslCerts', true);
+//$capabilities->setVersion("52.2.0");
+//$capabilities->setJavascriptEnabled(true);
+
+//$capabilities->setCapability(FirefoxDriver::PROFILE, ?);
+
+//$capabilities = DesiredCapabilities::chrome();
+//$driver = new \Facebook\WebDriver\Remote\RemoteWebDriver($host, $capabilities, 20000);
+//$driver = new \Facebook\WebDriver\Firefox\FirefoxDriver($host, $capabilities, 20000);
+//$driver = new \Facebook\WebDriver\Chrome\ChromeDriver($host, $capabilities, 20000);
 $driver = RemoteWebDriver::create($host, $capabilities, 5000);
+
+var_dump($driver);
 print $driver->getSessionID();
